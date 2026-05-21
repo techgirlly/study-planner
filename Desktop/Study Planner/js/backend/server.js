8ed95312ff6369645fa78788ed5f4cd5e1826a54
techgirlly <maryanney33399@gmail.com>
2026-05-21 16:51:09 +0300

    Fix server configuration and package setup for Railway deployment

--- Desktop/Study Planner/js/backend/server.js
+++ Desktop/Study Planner/js/backend/server.js
@@ -4,10 +4,20 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 
+
+dotenv.config();
+
+const app = express();
+const db = require("./config/db");
+
+const authRoutes = require("./routes/authRoutes");
+const taskRoutes = require("./routes/taskRoutes");
+const timetableRoutes = require("./routes/timetableRoutes");
+
 app.use(cors({
   origin: [
     "http://127.0.0.1:5500",
-    "http://localhost:5500",
+    "https://study-planner-brown-gamma.vercel.app",
     "https://study-planner-brown-gamma.vercel.app/",
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
@@ -15,18 +25,6 @@ app.use(cors({
 }));
 
 
-dotenv.config();
-
-const db = require("./config/db");
-
-
-const authRoutes = require("./routes/authRoutes");
-const taskRoutes = require("./routes/taskRoutes");
-const timetableRoutes = require("./routes/timetableRoutes");
-
-const app = express();
-
-app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
@@ -37,13 +35,6 @@ app.use("/api/timetable", timetableRoutes);
 app.get("/", (req, res) => {
   res.send("Study Planner API is running...");
 });
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
 app.get("/test-db", (req, res) => {
 
   db.query("SELECT 1", (err, result) => {
@@ -55,4 +46,10 @@ app.get("/test-db", (req, res) => {
     res.send("Database connected successfully");
   });
 
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
